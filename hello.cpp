a48991454fd9349367703e349e6c1399384bfb27
JORAWAR128 <joravarsingh2546@gmail.com>
2025-04-06 19:47:37 +0530

    update new greeting

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main(){
     cout<<"Hello , World"<<endl;
     cout<<"Namaste sir "<<endl;
-    cout<<"Vanakkam "<<endl;
+    cout<<"Vanakkam andi "<<endl;
     return 0;
 }
